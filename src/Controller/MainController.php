a26eeb9a9a964a02249f45470c7d58ddd266735d
khs56 <vishal.mudi@esd.ny.gov>
2022-05-21 22:35:38 -0400

    Update MainController.php

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,5 +17,15 @@ class MainController extends AbstractController
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
         ]);
+    }
+
+    /**
+     * @Route("/start/{name?}", name="start")
+     */
+    public function start(Request $request, $name){
+        $nr = random_int(0, 100);
+        return new Response(
+            '<h1>Hello ' . $name . '. Your lucky number is: ' . $nr . '</h1>'
+        );
     }
 }
